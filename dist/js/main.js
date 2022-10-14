c7235e0fc54ce724b8c8d32c485e21125401c8d3
Roman Bogoudinov <rb@Romans-Mac-Pro.local>
2022-10-14 10:40:07 +0300

    cv 10-2022

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,31 +1,31 @@
 // selecting DOM items
-const menuButton = document.querySelector('.menu-button');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-item');
+const menuButton = document.querySelector(".menu-button");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
 
 // Setting state of menu
 let showMenu = false;
 
-menuButton.addEventListener('click', toggleMenu);
+menuButton.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-    if(!showMenu) {
-        menuButton.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuBranding.classList.add('show');
-        navItems.forEach(item => item.classList.add('show')); 
-       // Set menu State
-       showMenu = true;
-    } else {
-        menuButton.classList.remove('close');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
-        navItems.forEach(item => item.classList.add('show')); 
-        // Set menu State
-       showMenu = false;
-    }
+  if (!showMenu) {
+    menuButton.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach((item) => item.classList.add("show"));
+    // Set menu State
+    showMenu = true;
+  } else {
+    menuButton.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach((item) => item.classList.add("show"));
+    // Set menu State
+    showMenu = false;
+  }
 }
